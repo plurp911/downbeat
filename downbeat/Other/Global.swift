190addcf3fe7fb4ac7a57e1ada3b13714428d2b2
Collin Howard <plurp911@gmail.com>
2019-01-31 17:31:02 -0800

    cleaned global

--- downbeat/Other/Global.swift
+++ downbeat/Other/Global.swift
@@ -20,19 +20,12 @@ var numberOfVerticalBricks: Int = 15
 
 let maxBullets: Int = 3
 
-//let screenRatio: CGFloat = 16 / 9
-//let screenRatio: CGFloat = 4 / 3
 let screenRatio: CGFloat = 16 / 15
-//let screenRatio: CGFloat = 1
 
 let chanceOfPowerup: CGFloat = 0.25
-//let chanceOfPowerup: CGFloat = 1
 
 // COLORS
 
-//let backgroundColor = UIColor.black
-
-//let backgroundColor = UIColor(red: 26 / 255, green: 26 / 255, blue: 26 / 255, alpha: 1)
 let backgroundColor = UIColor(red: 20 / 255, green: 20 / 255, blue: 20 / 255, alpha: 1)
 
 let mainButtonColor = UIColor.gray.withAlphaComponent(0.35)
@@ -48,52 +41,15 @@ let purchaseButtonOtherColor = UIColor(red: 25 / 255, green: 128 / 255, blue: 28
 let settingsButtonColor = UIColor(red: 25 / 255, green: 175 / 255, blue: 28 / 255, alpha: 1)
 let settingsButtonOtherColor = UIColor(red: 25 / 255, green: 128 / 255, blue: 28 / 255, alpha: 1)
 
-//let stageSelectViewColor = UIColor(red: 30 / 255, green: 161 / 255, blue: 35 / 255, alpha: 1)
 let stageSelectViewColor = UIColor(red: 25 / 255, green: 128 / 255, blue: 28 / 255, alpha: 1)
 
 let titleViewColor = UIColor(red: 14 / 255, green: 74 / 255, blue: 20 / 255, alpha: 1)
-//let titleViewColor = UIColor(red: 0 / 255, green: 0 / 255, blue: 0 / 255, alpha: 1)
-//let titleViewColor = stageSelectViewColor
-//let titleViewColor = UIColor(red: 20 / 255, green: 100 / 255, blue: 24 / 255, alpha: 1)
-
-//let gameViewColor = UIColor(red: 32 / 255, green: 56 / 255, blue: 236 / 255, alpha: 1)
-//let gameViewColor = UIColor(red: 101 / 255, green: 208 / 255, blue: 223 / 255, alpha: 1)
-//let gameViewColor = UIColor(red: 95 / 255, green: 63 / 255, blue: 20 / 255, alpha: 1)
-
-// BRICK
-
-//let gameViewColor = UIColor(red: 75 / 255, green: 125 / 255, blue: 223 / 255, alpha: 1)
-
-// FIRE
-
-//let gameViewColor = UIColor(red: 55 / 255, green: 0 / 255, blue: 0 / 255, alpha: 1)
-
-// TREE
-
-//let gameViewColor = UIColor(red: 47 / 255, green: 21 / 255, blue: 5 / 255, alpha: 1)
-//let gameViewColor = UIColor(red: 32 / 255, green: 14 / 255, blue: 3 / 255, alpha: 1)
-
-// METAL
-
-//let gameViewColor = UIColor(red: 152 / 255, green: 152 / 255, blue: 152 / 255, alpha: 1)
-
-// ICE
-
-//let gameViewColor = UIColor(red: 40 / 255, green: 174 / 255, blue: 190 / 255, alpha: 1)
-//let gameViewColor = UIColor(red: 34 / 255, green: 143 / 255, blue: 156 / 255, alpha: 1)
-//let gameViewColor = UIColor(red: 31 / 255, green: 127 / 255, blue: 139 / 255, alpha: 1)
-//var gameViewColor = UIColor(red: 27 / 255, green: 111 / 255, blue: 121 / 255, alpha: 1)
 
 // VARIABLES
-
-// var player1: AVAudioPlayer?
-// var player2: AVAudioPlayer?
 
 var players = [AVAudioPlayer?]()
 
 var musicPlayer: AVAudioPlayer?
-
-//var currentTrack: String = ""
 
 var isMusicMuted: Bool = false
 var isSoundMuted: Bool = false
@@ -121,9 +77,6 @@ var canMoveRight: Bool = true
 var canBeKnockedBack: Bool = true
 
 var canClimb: Bool = true
-
-//var canJump: Bool = true
-//var canShoot: Bool = true
 
 var isLeftPressed: Bool = false
 var isRightPressed: Bool = false
@@ -362,51 +315,6 @@ public func playSound(name: String) {
         
         do {
             
-            //        player1 = try AVAudioPlayer(contentsOf: url)
-            //        guard let player1 = player1 else { return }
-            //
-            //        player2 = try AVAudioPlayer(contentsOf: url)
-            //        guard let player2 = player2 else { return }
-            //
-            //        var volume: Float = 0
-            //
-            //        if name == "explosion" {
-            //            volume = 0.9
-            //        } else if name == "land" {
-            //            volume = 0.9
-            //        } else if name == "menu" {
-            //             volume = 0.9
-            //        } else if name == "shoot" {
-            //            volume = 0.9
-            //        } else {
-            //            print("-- PLAY SOUND ERROR --")
-            //        }
-            //
-            //        print("PLAYYINGGGGG")
-            //
-            //        if player1.isPlaying == false {
-            //
-            //            print("888888888888")
-            //
-            ////            player1.stop()
-            //
-            //            player1.volume = volume
-            //
-            //            player1.prepareToPlay()
-            //            player1.play()
-            //
-            //        } else if player2.isPlaying == false {
-            //
-            //            print("99999999999")
-            //
-            ////            player2.stop()
-            //
-            //            player2.volume = volume
-            //
-            //            player2.prepareToPlay()
-            //            player2.play()
-            //        }
-            
             var playersToRemove = [Int]()
             
             playersToRemove.removeAll()
@@ -460,12 +368,8 @@ public func playSound(name: String) {
             } else if name == "throw2" {
                 volume = 0.9
             } else {
-                print("-- PLAY SOUND ERROR --")
+                print("PLAY SOUND ERROR")
             }
-            
-            print("PLAYYINGGGGG")
-            
-            //            player1.stop()
             
             newPlayer.volume = volume
             
@@ -486,7 +390,6 @@ public func playSound(name: String) {
 extension String {
     
     func capitalizingFirstLetter() -> String {
-        //        return prefix(1).uppercased() + self.lowercased().dropFirst()
         return prefix(1).uppercased() + self.dropFirst()
     }
     
